01644d80c13a48d3c008a115d9f9c960462c699f
shipixian <shipixian_yewu@cmss.chinamobile.com>
2021-08-18 09:25:34 +0800

    distinguish the different tidb pools
    Signed-off-by: shipixian <shipixian_yewu@cmss.chinamobile.com>

--- pkg/tidb/proxy/backend/balancer.go
+++ pkg/tidb/proxy/backend/balancer.go
@@ -15,10 +15,9 @@
 package backend
 
 import (
-	"math/rand"
-	"time"
-
+	"fmt"
 	"github.com/pingcap/tidb/proxy/core/errors"
+	"sync/atomic"
 )
 
 func Gcd(ary []int) int {
@@ -50,7 +49,7 @@ func Gcd(ary []int) int {
 	return min
 }
 
-func (cluster *Cluster) InitBalancer() {
+func (cluster *Pool) InitBalancer() {
 	var sum int
 	cluster.LastTidbIndex = 0
 
@@ -58,9 +57,8 @@ func (cluster *Cluster) InitBalancer() {
 	sws := make([]int, 0, len(cluster.TidbsWeights))
 
 	for i := 0; i < len(cluster.TidbsWeights); i++ {
-			sws = append(sws, int(cluster.TidbsWeights[i]*10))
+		sws = append(sws, int(cluster.TidbsWeights[i]*10))
 	}
-
 
 
 	gcd := Gcd(sws)
@@ -76,47 +74,151 @@ func (cluster *Cluster) InitBalancer() {
 		}
 	}
 
-	//random order
+	//order by SWRR algorithm.
 	if 1 < len(cluster.TidbsWeights) {
-		r := rand.New(rand.NewSource(time.Now().UnixNano()))
-		for i := 0; i < sum; i++ {
-			x := r.Intn(sum)
-			temp := cluster.RoundRobinQ[x]
-			other := sum % (x + 1)
-			cluster.RoundRobinQ[x] = cluster.RoundRobinQ[other]
-			cluster.RoundRobinQ[other] = temp
-		}
+		cluster.RoundRobinQ = order(cluster.RoundRobinQ)
+		fmt.Println("cluster roundrobin is ", cluster.RoundRobinQ)
+		//r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		//for i := 0; i < sum; i++ {
+		//	x := r.Intn(sum)
+		//	temp := cluster.RoundRobinQ[x]
+		//	other := sum % (x + 1)
+		//	cluster.RoundRobinQ[x] = cluster.RoundRobinQ[other]
+		//	cluster.RoundRobinQ[other] = temp
+		//}
 	}
 }
 
-func (cluster *Cluster) GetNextTidb() (*DB, error) {
-	var index int
-	queueLen := len(cluster.RoundRobinQ)
-	if queueLen == 0 {
-		return nil, errors.ErrNoDatabase
-	}
-	if queueLen == 1 {
-		index = cluster.RoundRobinQ[0]
-		return cluster.Tidbs[index], nil
-	}
+type peer struct {
+	index int
+	current int
+	execpt int
+	weight int
+}
 
-	cluster.LastTidbIndex = cluster.LastTidbIndex % queueLen
 
-	var db *DB
-	for i := 0; i < len(cluster.RoundRobinQ); i++ {
-		index = cluster.RoundRobinQ[cluster.LastTidbIndex]
-		if len(cluster.Tidbs) <= index {
-			return nil, errors.ErrNoDatabase
+func order(tidbsWeights []int) []int {
+	var total int
+	var peers = make(map[int]*peer)
+	for key, value := range tidbsWeights {
+		peers[key] = &peer{
+			index: key,
+			weight: value,
+			execpt: value,
 		}
-		db = cluster.Tidbs[index]
-		cluster.LastTidbIndex++
-		cluster.LastTidbIndex = cluster.LastTidbIndex % queueLen
-		if db.state == Up {
-			return db, nil
+		total += value
+	}
+	result := make([]int, total, total)
+	var best *peer
+	for i := 0; i < total; i++ {
+		for _, v := range peers {
+			v.current += v.execpt
+			if best == nil || v.current > best.current {
+				best = v
+			}
 		}
+		if best == nil {
+			return result
+		}
+		best.current -= total
+		result[i] = best.index
+	}
+	return result
+}
+
+
+func (cluster *Cluster) GetNextTidb(lbIndicator string, cost int64) (*DB, error) {
+	//Distinguish SQL types based on costs
+	switch {
+	case cost <= 100 :
+		//Predicate SQL is belong to TP type
+		pool := cluster.BackendPools[TiDBForTP]
+
+		db, err := pool.GetNextDB(lbIndicator)
+		if err != nil {
+			return nil, err
+		}
+		if db.Self {
+			atomic.AddInt64(&cluster.ProxyNode.ProxyCost, cost)
+		} else {
+			atomic.AddInt64(&pool.Costs, cost)
+		}
+		return db, err
+
+	case cost > 10000:
+		//Predicate SQL is belong to Big AP type
+		//invoke grpc api of starting a new pod to handle this request.
+		//resp, err := server.ScaleSldb(cluster.Cfg.Namespace, cluster.Cfg.Name, DefaultBigSize, false, "bigcost")
+		//if err != nil {
+		//	return nil, errors.ErrGetConnTimeout
+		//}
+		//return GetBigCostDB(resp.GetAddress(), cluster.Cfg.User, cluster.Cfg.Password, "")
+
+	default:
+		//choose AP tidb pools
+		pool := cluster.BackendPools[TiDBForAP]
+		atomic.AddInt64(&pool.Costs, cost)
+		return pool.GetNextDB(lbIndicator)
 	}
 
 	return nil, errors.ErrAllDatabaseDown
+}
+
+func (cluster *Pool) GetNextDB(indicator string) (*DB, error) {
+	switch indicator {
+	case "qps":
+		var index int
+		queueLen := len(cluster.RoundRobinQ)
+		if queueLen == 0 {
+			return nil, errors.ErrNoDatabase
+		}
+		if queueLen == 1 {
+			index = cluster.RoundRobinQ[0]
+			return cluster.Tidbs[index], nil
+		}
+
+		cluster.LastTidbIndex = cluster.LastTidbIndex % queueLen
+
+		var db *DB
+		for i := 0; i < len(cluster.RoundRobinQ); i++ {
+			index = cluster.RoundRobinQ[cluster.LastTidbIndex]
+			if len(cluster.Tidbs) <= index {
+				return nil, errors.ErrNoDatabase
+			}
+			db = cluster.Tidbs[index]
+			cluster.LastTidbIndex++
+			cluster.LastTidbIndex = cluster.LastTidbIndex % queueLen
+			if db.state == Up {
+				return db, nil
+			}
+		}
+	case "cost":
+		//Check whether the number of tidb nodes exceeds 8.
+		//when less then 8, get tidb node of least costs.
+		//if len(cluster.Tidbs) < 8 {
+		//	var bestTidb *DB
+		//	for _, tidb := range cluster.Tidbs {
+		//		if bestTidb == nil || bestTidb.costs > tidb.costs {
+		//			bestTidb = tidb
+		//		}
+		//	}
+		//	return bestTidb, nil
+		//}
+
+		//todo: add get tidb node when the number of tidb nodes exceeds 8.
+	default:
+		return nil, errors.ErrAllDatabaseDown
+	}
+	return nil, errors.ErrInternalServer
+}
 
 
+func GetBigCostDB(addr string, user string, password string, dbName string) (*DB, error) {
+	db := new(DB)
+	db.addr = addr
+	db.user = user
+	db.password = password
+	db.db = dbName
+
+	return db, nil
 }
